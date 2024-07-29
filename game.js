eccf340e5e1fbe00f4f506d94ef899e78ef11705
Shan Torrejos <117603433+shantorrejos@users.noreply.github.com>
2024-07-29 17:29:03 +0800

    Initialize all required functions and scores

---
+++ game.js
@@ -0,0 +1,20 @@
+function getComputerChoice() {
+
+};
+
+function getHumanChoice() {
+
+};
+
+function playRound() {
+    
+};
+
+function playGame() {
+
+};
+
+let humanScore = 0;
+let computerScore = 0;
+
+
